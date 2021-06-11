c9f99707062986700081dcd95aebc85650701501
EC2 Default User <ec2-user@ip-172-31-81-92.ec2.internal>
2021-06-11 14:55:52 +0000

    [Finish]model association/ログイン不可

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root to: "homes#top"
   
   resources :users, only: [:show, :index]
+  resources :books
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_11_134222) do
+ActiveRecord::Schema.define(version: 2021_06_11_144415) do
+
+  create_table "books", force: :cascade do |t|
+    t.text "title"
+    t.text "opinion"
+    t.integer "user_id"
+    t.string "profile_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
