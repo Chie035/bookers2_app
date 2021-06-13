3d0805c44856eae5b2be14266aa331af2f73acf4
EC2 Default User <ec2-user@ip-172-31-81-92.ec2.internal>
2021-06-13 08:42:52 +0000

    フォーム不具合修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,15 +9,29 @@ class BooksController < ApplicationController
       @book = Book.new(book_params)
       @book.user_id = current_user.id
       @book.save
-      redirect_to books_path
+      redirect_to book_path(@book.id)
     end
   
   def show
     @book = Book.find(params[:id])
+    @book1 = Book.new
     @user = @book.user
   end
   
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
   
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,14 @@ class UsersController < ApplicationController
     
     def show
         @user = User.find(params[:id])
-        @books = Book.all
+        @book = Book.new
+        @books = @user.books.page(params[:page]).reverse_order
     end
     
     def index
         @users = User.page(params[:page]).reverse_order
         @user = current_user
-    
+        @book = Book.new
     end
     
 end
